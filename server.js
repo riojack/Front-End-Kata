8f06230c5254b24b1b41149e4a11d10e68cbaa5a
riojack <ayjay122@gmail.com>
2017-07-17 18:03:49 -0500

    Reformat code files

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 let express = require('express'),
-    app = express();
+  app = express();
 
 const port = 9300;
 
 app.get('/', (req, res) => {
-    res.sendFile('./example/index.html', {root: __dirname});
+  res.sendFile('./example/index.html', {
+    root: __dirname
+  });
 });
 
 app.listen(port, () => console.log(`Started example server on port ${port}.\nVisit http://localhost:${port}/ in your browser.`));
