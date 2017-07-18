bd6b1df47e781ef818471ce5a4baf5a40a5ffd66
riojack <ayjay122@gmail.com>
2017-07-17 19:23:55 -0500

    First two tests for BannerMenu component

---
+++ src/components/BannerMenu.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class BannerMenu extends React.Component {
+  render() {
+    return <div className="banner-menu" />;
+  }
+}
+
+export default BannerMenu;
--- test/components/BannerMenu.spec.js
+++ test/components/BannerMenu.spec.js
@@ -1,7 +1,21 @@
+import React from 'react';
 import {expect} from 'chai';
+import {shallow} from 'enzyme';
+import BannerMenu from '../../src/components/BannerMenu';
 
 describe('Banner Menu Component', () => {
-  it('should pass this test', () => {
-    expect(true).to.equal(true);
+  let renderedInstance;
+
+  beforeEach(() => {
+    renderedInstance = shallow(<BannerMenu />);
+  });
+
+  it('should be a DIV', () => {
+    expect(renderedInstance.type()).to.equal('div');
+  });
+
+  it('should have a css class of "banner-menu"', () => {
+    expect(renderedInstance.props()).to.have.property('className')
+      .that.equals('banner-menu');
   });
 });
