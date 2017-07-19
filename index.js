a26fb50d3eca6e124472f97df1e0f39a15dfd45f
riojack <ayjay122@gmail.com>
2017-07-19 08:04:56 -0500

    Render Application component

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 require('./src/sass/application.scss');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Application from './src/components/Application';
+
+window.Pillar = {
+  doRender: function(doc) {
+    let element = React.createElement(Application, {});
+
+    ReactDOM.render(element, doc.getElementById('example-container'));
+  }
+};
