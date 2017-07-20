bc1b03604a1b31285de7144d6f28cb1ff0f9b5c3
riojack <ayjay122@gmail.com>
2017-07-20 18:27:58 -0500

    Add text to shoutout card

--- test/components/ShoutoutCard.spec.js
+++ test/components/ShoutoutCard.spec.js
@@ -20,4 +20,14 @@ describe('ShoutoutCard component', () => {
     expect(renderedInstance.props()).to.have.property('className')
       .that.equals('shoutout-card');
   });
+
+  describe('children', () => {
+    it('should have the expected children', () => {
+      expect(renderedInstance.contains([
+        <span className="first-line">Are you ready to change?</span>,
+        <span className="noticeable-one-liner">...your industry is.</span>,
+        <span className="elevator-pitch"><strong>Good thing Pillar's on a first name basis with Technology.</strong><br />In fact, we're a tech company taking marketing and strategy by the horns. Your traditional marketing company talks about integrating technology with these things&mdash;they talk&mdash;but we do it. And we do it better than anyone else.  Because our technology is our game, and our team is made up of the best players.</span>
+      ])).to.equal(true);
+    });
+  });
 });
