342d6838a18e9dd12622b547df8e2bdc609ee818
riojack <ayjay122@gmail.com>
2017-07-20 10:16:52 -0500

    Get carousel working in example

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import verbs from './src/verbs';
+import timer from './src/timer';
 
+window.PillarHelpers = {
+  timer: timer
+};
 window.Pillar = Object.assign({}, verbs);
