ebf756202b339722217f74a6555b95de850bee1c
riojack <ayjay122@gmail.com>
2017-07-20 20:01:30 -0500

    Add image to quickcard component and left or right direction modes

--- src/components/QuickCard.jsx
+++ src/components/QuickCard.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
+function getDirectionMode(props) {
+  return props.direction_mode === 'right' ? 'right' : 'left';
+}
+
 class QuickCard extends React.Component {
   render() {
-    return <div className="quick-card">
-      <span className="interrogative-quote">{this.props.interrogative_sentence}</span>
-      <span className="eye-catcher">{this.props.eye_catcher_sentence}</span>
-      <span className="blurb">{this.props.blurb_sentence}</span>
+    return <div className="quick-card" data-direction-mode={getDirectionMode(this.props)}>
+      <img src={this.props.image} />
+      <div className="block-of-quotes">
+        <span className="interrogative-quote">{this.props.interrogative_sentence}</span>
+        <span className="eye-catcher">{this.props.eye_catcher_sentence}</span>
+        <span className="blurb">{this.props.blurb_sentence}</span>
+      </div>
     </div>;
   }
 }
--- test/components/QuickCard.spec.js
+++ test/components/QuickCard.spec.js
@@ -7,13 +7,18 @@ describe('QuickCard component', () => {
   let props, 
     renderedInstance;
 
+  function doRender(props) {
+    return shallow(<QuickCard {...props} />);
+  }
+
   beforeEach(() => {
     props = {
+      image: 'some.location/image.png',
       interrogative_sentence: 'how now brown cow',
       eye_catcher_sentence: 'whoa-ho, no bro!',
       blurb_sentence: 'foo bar baz'
     };
-    renderedInstance = shallow(<QuickCard {...props} />);
+    renderedInstance = doRender(props);
   });
 
   it('should be a DIV', () => {
@@ -25,12 +30,36 @@ describe('QuickCard component', () => {
       .that.equals('quick-card');
   });
 
+  describe('left or right modes', () => {
+    it('should render left mode by default if props.direction_mode is not provided', () => {
+      expect(renderedInstance.props()).to.have.property('data-direction-mode')
+        .that.equals('left');
+    });
+
+    it('should render right mode if props.direction_mode is the value "right"', () => {
+      props.direction_mode = 'right';
+      renderedInstance = doRender(props);
+      expect(renderedInstance.props()).to.have.property('data-direction-mode')
+        .that.equals('right');
+    });
+
+    it('should render left mode if props.direction_mode is anything other than "left" or "right"', () => {
+      props.direction_mode = 'foo';
+      renderedInstance = doRender(props);
+      expect(renderedInstance.props()).to.have.property('data-direction-mode')
+        .that.equals('left');
+    });
+  });
+
   describe('children', () => {
     it('should render its children as expected', () => {
       expect(renderedInstance.contains([
-        <span className="interrogative-quote">{props.interrogative_sentence}</span>,
-        <span className="eye-catcher">{props.eye_catcher_sentence}</span>,
-        <span className="blurb">{props.blurb_sentence}</span>
+        <img src={props.image} />,
+        <div className="block-of-quotes">
+          <span className="interrogative-quote">{props.interrogative_sentence}</span>
+          <span className="eye-catcher">{props.eye_catcher_sentence}</span>
+          <span className="blurb">{props.blurb_sentence}</span>
+        </div>
       ])).to.equal(true);
     });
   });
