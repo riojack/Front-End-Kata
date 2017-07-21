253af5015a81a264bc4a5e5a9374ef39a72f32e3
riojack <ayjay122@gmail.com>
2017-07-20 20:19:47 -0500

    Integrate 3 QuickCard components into Application component

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import BannerMenu from './BannerMenu';
 import ShoutoutCard from './ShoutoutCard';
+import QuickCard from './QuickCard';
 
 class Application extends React.Component {
   render() {
     return <div className="application-container">
       <BannerMenu {...this.props.banner_props} />
       <ShoutoutCard />
+      <QuickCard 
+          image="assets/e-image-1_2017-07-05/e-image-1.png" 
+          interrogative_sentence="What We Do" 
+          eye_catcher_sentence="Turn your fears into fuel." 
+          blurb_sentence="Industries are being disrupted every day.  We create solutions to disrupt your competition before it disrupts you.  Our strategists, technologies and designers collaborate to create software-fueled digital experiences that drive business further." />
+        <QuickCard 
+          image="assets/e-image-2_2017-07-05/e-image-2.png" 
+          interrogative_sentence="How We Do It" 
+          eye_catcher_sentence="Fusion. It's how we bring the heat." 
+          blurb_sentence="What is it?  It's the combustion of technology, strategy, innovation, DX and marketing.  It's what delivers the solution to your most difficult business problems." />
+        <QuickCard 
+          image="assets/e-image-3_2017-07-05/e-image-3.png" 
+          interrogative_sentence="Where We Forge the Future" 
+          eye_catcher_sentence="Here, every idea has the ability to turn into a great one." 
+          blurb_sentence="A philosophy&mdash;sometimes in four walls, sometimes not.  It's the business strategists, creatives, software artisans and clients collaborating to solve business problems in a no-constraints think tank.  It's where and how we work&mdash;mentally and physicall.  Wherever our work takes us, we bring the Forge mindset with us." />
     </div>;
   }
 }
--- test/components/Application.spec.js
+++ test/components/Application.spec.js
@@ -4,6 +4,7 @@ import {shallow} from 'enzyme';
 import Application from '../../src/components/Application';
 import BannerMenu from '../../src/components/BannerMenu';
 import ShoutoutCard from '../../src/components/ShoutoutCard';
+import QuickCard from '../../src/components/QuickCard';
 
 describe('ApplicationComponent', () => {
   let props, 
@@ -52,6 +53,28 @@ describe('ApplicationComponent', () => {
 
     it('should have a ShoutoutCard child', () => {
       expect(shoutoutCard).to.have.length(1);
+    });
+  });
+
+  describe('QuickCard children', () => {
+    it('should have 3 QuickCard children', () => {
+      expect(renderedInstance.contains([
+        <QuickCard 
+          image="assets/e-image-1_2017-07-05/e-image-1.png" 
+          interrogative_sentence="What We Do" 
+          eye_catcher_sentence="Turn your fears into fuel." 
+          blurb_sentence="Industries are being disrupted every day.  We create solutions to disrupt your competition before it disrupts you.  Our strategists, technologies and designers collaborate to create software-fueled digital experiences that drive business further." />,
+        <QuickCard 
+          image="assets/e-image-2_2017-07-05/e-image-2.png" 
+          interrogative_sentence="How We Do It" 
+          eye_catcher_sentence="Fusion. It's how we bring the heat." 
+          blurb_sentence="What is it?  It's the combustion of technology, strategy, innovation, DX and marketing.  It's what delivers the solution to your most difficult business problems." />,
+        <QuickCard 
+          image="assets/e-image-3_2017-07-05/e-image-3.png" 
+          interrogative_sentence="Where We Forge the Future" 
+          eye_catcher_sentence="Here, every idea has the ability to turn into a great one." 
+          blurb_sentence="A philosophy&mdash;sometimes in four walls, sometimes not.  It's the business strategists, creatives, software artisans and clients collaborating to solve business problems in a no-constraints think tank.  It's where and how we work&mdash;mentally and physicall.  Wherever our work takes us, we bring the Forge mindset with us." />
+      ])).to.equal(true);
     });
   });
 });
