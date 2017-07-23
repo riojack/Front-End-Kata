0150498aacd3796718cdc7e0fd18038d6bb07fef
riojack <ayjay122@gmail.com>
2017-07-23 09:51:33 -0500

    Fill out Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,33 @@ import React from 'react';
 
 class Footer extends React.Component {
   render() {
-    return <div className="footer" />;
+    return <div className="footer">
+      <div className="site-map-and-social-links">
+        <ul className="site-map">
+          <li>About</li>
+          <li>What We Do</li>
+          <li>How We Do It</li>
+          <li>Where We Work</li>
+        </ul>
+        <ul className="site-map">
+          <li>Portfolio</li>
+          <li>Awards</li>
+          <li>Careers</li>
+        </ul>
+        <ul className="site-map">
+          <li>Events</li>
+          <li>Contact</li>
+        </ul>
+        <ul className="social-links">
+          <li><img className="to-facebook" /></li>
+          <li><img className="to-snapchat" /></li>
+          <li><img className="to-linkedin" /></li>
+          <li><img className="to-twitter" /></li>
+          <li><img className="to-youtube" /></li>
+        </ul>
+        <span className="copyright">Copyright &copy; 2017 Pillar Technology Group, LLC</span>
+      </div>
+    </div>;
   }
 }
 
--- test/components/Footer.spec.js
+++ test/components/Footer.spec.js
@@ -24,4 +24,39 @@ describe('Footer component', () => {
     expect(renderedInstance.props()).to.have.property('className')
       .that.equals('footer');
   });
+
+  describe('site map and social links', () => {
+    it('should have a div with css class of "site-map-and-social-links" and the expected content', () => {
+      let siteAndSocial = renderedInstance.children('.site-map-and-social-links');
+
+      expect(siteAndSocial).to.have.length(1);
+      expect(siteAndSocial.type()).to.equal('div');
+
+      expect(siteAndSocial.contains([
+        <ul className="site-map">
+          <li>About</li>
+          <li>What We Do</li>
+          <li>How We Do It</li>
+          <li>Where We Work</li>
+        </ul>,
+        <ul className="site-map">
+          <li>Portfolio</li>
+          <li>Awards</li>
+          <li>Careers</li>
+        </ul>,
+        <ul className="site-map">
+          <li>Events</li>
+          <li>Contact</li>
+        </ul>,
+        <ul className="social-links">
+          <li><img className="to-facebook" /></li>
+          <li><img className="to-snapchat" /></li>
+          <li><img className="to-linkedin" /></li>
+          <li><img className="to-twitter" /></li>
+          <li><img className="to-youtube" /></li>
+        </ul>,
+        <span className="copyright">Copyright &copy; 2017 Pillar Technology Group, LLC</span>
+      ])).to.equal(true);
+    });
+  });
 });
